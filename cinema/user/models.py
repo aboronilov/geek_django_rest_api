67233541ee04e6b3318cb16ce3cba650e76c516d
= <=>
2022-03-01 09:26:48 +0200

    lesson_1

--- cinema/user/models.py
+++ cinema/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class User(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid4)
+    username = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
--- cinema/user/views.py
+++ cinema/user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserAPIView(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
